90789ff65bbec359e6224a09c6ae73faabad59fc
annjhwang <annhwang@college.harvard.edu>
2016-11-09 20:12:30 -0500

    adding SLS project with files

---
+++ sls12.php
@@ -0,0 +1,150 @@
+<html>
+  <head>
+		<title>Annie Hwang</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/animate.min.css" type="text/css">
+
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+	</head>
+  <body>
+  	<!-- Wrapper-->
+	<div>
+		<div id="wrapper">
+			<!-- Nav -->
+			<nav id="nav">
+				<a href="#me" class="icon fa-home active"><span>Home</span></a>
+				<a href="#work" class="icon fa-folder"><span>Project</span></a>
+				<a href="#tree" class="icon fa-folder"><span>Tree</span></a>
+			</nav>
+
+			<div id="main">
+				<!-- Me -->
+				<article id="me" class="panel">
+					<header>
+						<h1>SLS 12 Final Project</h1>
+						<p>Physical and Behavioral Characteristics</p>
+					</header>
+					<img src="images/facebook1.jpg" width="" height="" />
+					<a href="#work" class="jumplink pic">
+						<span class="arrow icon fa-chevron-right"><span>See my work</span></span>
+						<img src="images/facebook1.jpg" alt="" />
+					</a>
+				</article>
+				<!-- Me -->
+				<article id="tree" class="panel">
+					<!-- college tree graph -->
+	                <div class="space-at-top box tree-vis">
+	                    <h2 id="yellow-highlight" class="text-center" style="margin-bottom: 10px;">Family Lineage</h2>
+	                    <i class="text-faded">Click on one of the below nodes to expand the tree. Clicking on an already expanded node will contract the tree.</i>
+	                    <div id="college-tree" class="wow fadeIn" data-wow-delay="0.5s"></div>
+	                </div>
+            	</article>
+
+				<article id="work" class="panel">
+					<header>
+						<h2>Project</h2>
+					</header>
+					<p>I am a summation of my past experiences.</p>
+					<p>	
+					This is an interactive website that helps you learn both about me, and concepts that I have learned in my SLS class. This website covers concepts of nature vs. nurture, dominance in traits, and behavioral genetics. My web page showcases 10-12 physical and 3-5 behavioral qualities of me (with visuals), and attributing each to either Nature or Nurture. Some examples for physical qualities include: brown eyes, dimples, height, tongue rolling, etc. Some examples for behavioral qualities include: extroverted, stubborn, techy, etc. In the case that the quality is attributed to Nature, I will also explain where that same trait can be found in parents/grandparents with their pictures. While doing so, I’ll also explain the recessive/dominance of the trait. If the quality is attributed to Nurture, I will show how my past experiences and lifestyle has made me to who I am today with those certain qualities. If a quality is influenced by both Nature and Nurture, also address that in explanation of the traits.
+					</p>
+					<h4>Physical Traits</h4>
+
+
+					<section>
+						<div class="row">
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Dimples</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/shortshot.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Short: 5' 2''</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Eyes: color, shape</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Attached earlobes</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/shortshot.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Straight hair</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Thin lips</a>
+							</div>
+					</section>
+
+					<section>		
+						<h4>Behavioral Traits</h4>
+						<div class="row">
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Fearless</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Stubborn</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Extroverted</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/dimples.png"  alt=""></a>
+								<a href="CS171-Project/curry_index.php">Techy</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Stubborn</a>
+							</div>
+							<div class="2u 6u$(mobile)">
+								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+								<a href="CS171-Project/curry_index.php">Extroverted</a>
+							</div>
+					</section>
+				</article>
+
+			</div>
+
+		</div>
+	</div>
+
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/skel-viewport.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="assets/js/main.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+    <!-- Plugin JavaScript -->
+
+    <script src="assets/js/d3.min.js"></script>
+    <script src="assets/js/hexbin.js"></script>
+    <script src="assets/js/d3.chart.js"></script>
+
+
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/jquery.fittext.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+
+	<script src="assets/js/ancestorTree.js"></script>
+
+	
+  </body>
+</html>
