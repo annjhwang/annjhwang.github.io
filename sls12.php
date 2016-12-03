dbc47d6c1563ddf0db0ceed6e7f6cf0ad3725c2f
annjhwang <annhwang@college.harvard.edu>
2016-12-03 14:40:47 -0500

    SLS12 project draft 1

--- sls12.php
+++ sls12.php
@@ -13,110 +13,126 @@
 	</head>
   <body>
   	<!-- Wrapper-->
-	<div>
-		<div id="wrapper">
-			<!-- Nav -->
-			<nav id="nav">
-				<a href="#me" class="icon fa-home active"><span>Home</span></a>
-				<a href="#work" class="icon fa-folder"><span>Project</span></a>
-				<a href="#tree" class="icon fa-folder"><span>Tree</span></a>
-			</nav>
+	<div id="wrapper">
+		<!-- Nav -->
+		<nav id="nav">
+			<a href="#me" class="icon fa-home active"><span>Home</span></a>
+			<a href="#work" class="icon fa-folder"><span>Project</span></a>
+			<a href="#tree" class="icon fa-folder"><span>Tree</span></a>
+			<a href="#heritability" class="icon fa-folder"><span>Heritability</span></a>
+			<a href="#analysis" class="icon fa-folder"><span>Final Analysis</span></a>
+		</nav>
 
-			<div id="main">
-				<!-- Me -->
-				<article id="me" class="panel">
-					<header>
-						<h1>SLS 12 Final Project</h1>
-						<p>Physical and Behavioral Characteristics</p>
-					</header>
-					<img src="images/facebook1.jpg" width="" height="" />
-					<a href="#work" class="jumplink pic">
-						<span class="arrow icon fa-chevron-right"><span>See my work</span></span>
-						<img src="images/facebook1.jpg" alt="" />
-					</a>
-				</article>
-				<!-- Me -->
-				<article id="tree" class="panel">
-					<!-- college tree graph -->
-	                <div class="space-at-top box tree-vis">
-	                    <h2 id="yellow-highlight" class="text-center" style="margin-bottom: 10px;">Family Lineage</h2>
-	                    <i class="text-faded">Click on one of the below nodes to expand the tree. Clicking on an already expanded node will contract the tree.</i>
-	                    <div id="college-tree" class="wow fadeIn" data-wow-delay="0.5s"></div>
-	                </div>
-            	</article>
+		<div id="main">
+			<!-- Me -->
+			<article id="me" class="panel">
+				<header>
+					<h1>SLS 12 Final Project</h1>
+					<p>Physical and Behavioral Characteristics</p>
+				</header>
+				<img src="images/facebook1.jpg" width="" height="" />
+				<a href="#work" class="jumplink pic">
+					<span class="arrow icon fa-chevron-right"><span>See my work</span></span>
+					<img src="images/facebook1.jpg" alt="" />
+				</a>
+			</article>
+			<!-- Me -->
+			<article id="tree" class="panel">
+				<!-- college tree graph -->
+                    <h2 id="yellow-highlight" class="text-center" style="margin-bottom: 10px;">Family Lineage</h2>
+                    <i class="text-faded">Click on one of the below nodes to expand the tree. Clicking on an already expanded node will contract the tree.</i>
+                    <div id="college-tree" class="wow fadeIn" data-wow-delay="0.5s"></div>
+        	</article>
 
-				<article id="work" class="panel">
-					<header>
-						<h2>Project</h2>
-					</header>
-					<p>I am a summation of my past experiences.</p>
-					<p>	
-					This is an interactive website that helps you learn both about me, and concepts that I have learned in my SLS class. This website covers concepts of nature vs. nurture, dominance in traits, and behavioral genetics. My web page showcases 10-12 physical and 3-5 behavioral qualities of me (with visuals), and attributing each to either Nature or Nurture. Some examples for physical qualities include: brown eyes, dimples, height, tongue rolling, etc. Some examples for behavioral qualities include: extroverted, stubborn, techy, etc. In the case that the quality is attributed to Nature, I will also explain where that same trait can be found in parents/grandparents with their pictures. While doing so, I’ll also explain the recessive/dominance of the trait. If the quality is attributed to Nurture, I will show how my past experiences and lifestyle has made me to who I am today with those certain qualities. If a quality is influenced by both Nature and Nurture, also address that in explanation of the traits.
-					</p>
-					<h4>Physical Traits</h4>
+			<article id="work" class="panel">
+				<header>
+					<h2>Introduction</h2>
+				</header>
+				<p>	
+				Hi! I'm Annie. This is an interactive website that helps you learn both about me, and concepts that I have learned in my "Understanding Darwinism" class at Harvard. This website covers concepts of Nature vs. Nurture, heritability, and a little bit of behavioral genetics. My web page showcases 6 physical and 6 behavioral qualities of me (with visuals). Some examples for physical qualities include: brown eyes, dimples, height, tongue rolling, etc. Some examples for behavioral qualities include: extroversion, stubbornness, being tech-savvy, etc. 
+				</p>
+				<p>
+				While examining each trait, I first address the heritability to start the discussion of whether nature or nurture has played a more significant role in the development of the trait. Whether nature or nurture played a more signifant role in the development of that trait, I will discuss the roots and causes  (experience for Nurture and traits in parents for Nature). If the trait has been influenced by nurture, I discuss past experiences and lifestyle that has made me to who I am today with those certain qualities.
+				</p>
+				<h4>Physical Traits</h4>
 
 
-					<section>
-						<div class="row">
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Dimples</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/shortshot.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Short: 5' 2''</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Eyes: color, shape</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Attached earlobes</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/shortshot.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Straight hair</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Thin lips</a>
-							</div>
-					</section>
+				<section>
+					<div class="row">
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
+							<a href="sls12traits/physical/dimples.php">Dimples</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/shortshot.png" alt=""></a>
+							<a href="sls12traits/physical/short.php">Short: 5' 2''</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
+							<a href="sls12traits/physical/eyecolor.php">Eyes: color, shape</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/attachedear.jpg" alt=""></a>
+							<a href="sls12traits/physical/earlobe.php">Attached earlobes</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/hair.png" alt=""></a>
+							<a href="sls12traits/physical/straighthair.php">Straight hair</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/thinlip.png" alt=""></a>
+							<a href="sls12traits/physical/thinlips.php">Thin lips</a>
+						</div>
+				</section>
 
-					<section>		
-						<h4>Behavioral Traits</h4>
-						<div class="row">
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/dimples.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Fearless</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Stubborn</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Extroverted</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/dimples.png"  alt=""></a>
-								<a href="CS171-Project/curry_index.php">Techy</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Stubborn</a>
-							</div>
-							<div class="2u 6u$(mobile)">
-								<a href="#" class="image fit"><img src="images/eyes.png" alt=""></a>
-								<a href="CS171-Project/curry_index.php">Extroverted</a>
-							</div>
-					</section>
-				</article>
+				<section>		
+					<h4>Behavioral Traits</h4>
 
-			</div>
+					<p>For the behavioral traits, I mainly cover the 5 Big Personality Traits (Neuroticm, Openness, Extroversion, Agreeableness, and Conscientiousness) that psychologists often use to define someone's personality. </p>
+					<div class="row">
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/neuro.png" alt=""></a>
+							<a href="sls12traits/behavioral/neuroticism.php">Neuroticism</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/openness.png" alt=""></a>
+							<a href="sls12traits/behavioral/openness.php">Openness</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/extroverted.crdownload" alt=""></a>
+							<a href="sls12traits/behavioral/extrovert.php">Extroverted</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/agreeable.png"  alt=""></a>
+							<a href="sls12traits/behavioral/agreeableness.php">Agreeableness</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/stubborn.png" alt=""></a>
+							<a href="sls12traits/behavioral/stubborn.php">Stubborn</a>
+						</div>
+						<div class="2u 6u$(mobile)">
+							<a href="#" class="image fit"><img src="images/conscience.png" alt=""></a>
+							<a href="sls12traits/behavioral/conscience.php">Conscientious</a>
+						</div>
+				</section>
+			</article>
+
+			<article id="heritability" class="panel">
+				<!-- college tree graph -->
+                    <h2 id="yellow-highlight" class="text-center" style="margin-bottom: 10px;">Heritability Graph</h2>
+
+        	</article>
+
+        	<article id="analysis" class="panel">
+				<!-- college tree graph -->
+                    <h2 id="yellow-highlight" class="text-center" style="margin-bottom: 10px;">Final Analysis</h2>
+                    <p>Behavioral - tends to have a lower hertiability score,; most physical traits have high heritability score.
+                    	some of them are "irregular" dominant traits. 
+                    </p>
+        	</article>
 
 		</div>
+
 	</div>
 
 
@@ -130,8 +146,7 @@
 	<!-- Bootstrap Core JavaScript -->
     <script src="assets/js/bootstrap.min.js"></script>
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
+    
     <!-- Plugin JavaScript -->
 
     <script src="assets/js/d3.min.js"></script>
