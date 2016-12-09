f4795a1cf7c3896f94a70b7943375520afe34ea5
annjhwang <annhwang@college.harvard.edu>
2016-12-08 20:07:34 -0500

    adding files

---
+++ sls12traits/physical/bmi.php
@@ -0,0 +1,62 @@
+<html>
+  <head>
+		<title>Annie Hwang</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="/assets/css/main.css" />
+		<link rel="stylesheet" href="/assets/css/animate.min.css" type="text/css">
+
+		<noscript><link rel="stylesheet" href="/assets/css/noscript.css" /></noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+	</head>
+  	<!-- Wrapper-->
+	<div id="container">
+  		<center><h2>Body Mass Index</h2>
+  		<img src="/images/bmi.png" width="210" height="180" margin=60/>
+ 		</center>
+	<p>
+		Heritability: <a href="http://www.nature.com.ezp-prod1.hul.harvard.edu/ng/journal/v47/n10/full/ng.3390.html">0.3-0.6(studies vary)</a><br>
+		Nature's Influence: Mom has BMI of around 26 (healthy for her age), Dad: 25 (healthy)<br>
+		Nurture's influence on trait: Healthy diet, exercise, stress level <br>
+		Analysis of trait: I have BMI of 22.0, while my parents have BMIs of around 25. Studies show that BMI has a slightly lower heritability index than most 
+		other physical traits (i.e. height that has a heritability
+		index of around 0.8). This is particularly because various "nurture" factors like eating and exercising habits are key influencers for
+		one's body mass index. However, the reason the heritability score isn't lower is because these factors that we would initially think of as
+		"nurture" are most likely influenced by other behavioral traits we may have that have a higher heritability score. For example, individuals with
+		a low self-disicpline value, which has a relatively high heritablity index <a href="http://download.springer.com.ezp-prod1.hul.harvard.edu/static/pdf/944/art%253A10.1023%252FA%253A1012294324713.pdf?originUrl=http%3A%2F%2Flink.springer.com%2Farticle%2F10.1023%2FA%3A1012294324713&token2=exp=1481226396~acl=%2Fstatic%2Fpdf%2F944%2Fart%25253A10.1023%25252FA%25253A1012294324713.pdf%3ForiginUrl%3Dhttp%253A%252F%252Flink.springer.com%252Farticle%252F10.1023%252FA%253A1012294324713*~hmac=db261d3e5f344f06dcafa1e4fee964399b3a206d5c4ca6b9b7d94c3421799a81"
+		>(0.28)</a> for a behavioral trait, would most likely have a higher BMI. Hence, it is 
+		very hard to distinguish between nurture's and nature's influence on traits like BMI, because they are inherently interdependent. BMI is also race
+		and sex dependent, which also highlights nature's effect on BMI.
+		
+	</p>
+</div>
+	  	<!-- Scripts -->
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/skel.min.js"></script>
+	<script src="/assets/js/skel-viewport.min.js"></script>
+	<script src="/assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="/assets/js/main.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+    <script src="/assets/js/bootstrap.min.js"></script>
+
+    <script type="/text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+    <!-- Plugin JavaScript -->
+
+    <script src="/assets/js/d3.min.js"></script>
+    <script src="/assets/js/hexbin.js"></script>
+    <script src="/assets/js/d3.chart.js"></script>
+
+
+    <script src="/assets/js/jquery.easing.min.js"></script>
+    <script src="/assets/js/jquery.fittext.js"></script>
+    <script src="/assets/js/wow.min.js"></script>
+
+	<script src="/assets/js/ancestorTree.js"></script>
+
+	
+  </body>
+</html>
---
+++ sls12traits/physical/eczema.php
@@ -0,0 +1,57 @@
+<html>
+  <head>
+		<title>Annie Hwang</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="/assets/css/main.css" />
+		<link rel="stylesheet" href="/assets/css/animate.min.css" type="text/css">
+
+		<noscript><link rel="stylesheet" href="/assets/css/noscript.css" /></noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+	</head>
+  	<!-- Wrapper-->
+
+  	<div id="container">
+  		<center><h2>Eczema</h2>
+  		<img src="/images/eczema.png" width="240" height="170" margin=60/>
+ 		</center>
+	
+	<p>Heritability: <a href="http://www.twinsuk.ac.uk/wp-content/uploads/2012/06/Bataille-JEADerm-2012.pdf">Around 0.6</a><br>
+		Nature's Influence: Neither parents have eczema currently, but father used to when an infant<br>
+		Nurture's Influence: Sleeping habits, nutrition, exercise, stress<br>
+		Analysis: Twin studies in Denmark show that the heritabilty index of eczema and other skin diseases are affected both by nature and nurture.
+		I have eczema around my arms, and they get a lot worse when I eat unhealthy or don't get enough sleep. Neither of my parents have
+		eczema. Eczema is an interesting physical trait to examine, because the heritability index for eczema in children is around 0.9, while
+		the heritability index of eczema in adults are around 0.6. This shows that one's environment and habits can greatly help cure eczema
+		over the years with good care. </p>
+	</div>
+	  	<!-- Scripts -->
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/skel.min.js"></script>
+	<script src="/assets/js/skel-viewport.min.js"></script>
+	<script src="/assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="/assets/js/main.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+    <script src="/assets/js/bootstrap.min.js"></script>
+
+    <script type="/text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+    <!-- Plugin JavaScript -->
+
+    <script src="/assets/js/d3.min.js"></script>
+    <script src="/assets/js/hexbin.js"></script>
+    <script src="/assets/js/d3.chart.js"></script>
+
+
+    <script src="/assets/js/jquery.easing.min.js"></script>
+    <script src="/assets/js/jquery.fittext.js"></script>
+    <script src="/assets/js/wow.min.js"></script>
+
+	<script src="/assets/js/ancestorTree.js"></script>
+
+	
+  </body>
+</html>
